bd659eddbe63c08faa49a4c2811c457f16260bfd
David Morrow <dperrymorrow@gmail.com>
2019-02-08 09:14:41 -0800

    adding init function and registry in npmrc

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 "use strict";
 
+const fs = require("fs");
+const path = require("path");
+const logger = require("../lib/logger")("Dev Boost CLI");
 const devServer = require("../lib/dev-server");
 const build = require("../lib/build");
+const defaultConfig = Object.assign(require("../lib/config"), {
+  sourceDir: "src/",
+  buildDir: "docs/",
+});
+const configPath = path.join(process.cwd(), ".dev-boost.json");
 
 const args = process.argv;
 const commands = {
   serve: devServer.serve,
   build: build.generate,
+  init() {
+    if (fs.existsSync(configPath))
+      logger.warn("there is already a .dev-boost.json file presen)t, skipping");
+    else fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
+  },
 };
 
 let command = args.pop();
